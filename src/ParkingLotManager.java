ae25fa4fd6b0b3f33581f4951a8abba77bb6f5dd
Ramesh Rs <rameshrs@centizen.com>
2025-02-17 12:08:04 +0530

    first commit

---
+++ src/ParkingLotManager.java
@@ -0,0 +1,15 @@
+public class ParkingLot {
+
+    private int totalSpot ;
+    private int suvSpot;
+    private int hatchBackSport;
+
+
+    public ParkingLot(int totalSpot , int suvSpot , int hatchBackSport){
+        this.totalSpot = totalSpot;
+        this.suvSpot = suvSpot;
+        this.hatchBackSport = hatchBackSport;
+    }
+
+
+}
